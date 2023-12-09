df54cddf73860d38e1b89458c98b2b2f83921811
Philip Mills <philip.mills1998@outlook.com>
2023-12-09 11:59:28 +1100

    Implement mark_todo function

--- todo_functions.py
+++ todo_functions.py
@@ -14,9 +14,35 @@ def add_todo(file_name):
 
 def remove_todo(file_name): 
     print("Remove TODO item.")
+    todo_name = input("Enter a todo that you want to remove: ")
+    # copy all the contents of the csv into a new csv
+    # while doign this, we constantly check for the condition
+    # when we encounter ther todo to be removed, we don't copy that one
+    # the final new todo will be written in the csv file
+    todo_lists = []
+    with open(file_name, "r") as f: 
+        reader = csv.reader(f)
+        for row in reader: 
+            if (todo_name != row[0]): 
+                todo_lists.append(row)
+    with open(file_name, "w") as f: 
+        writer = csv.writer(f)
+        writer.writerows(todo_lists)
 
 def mark_todo(file_name):
     print("Mark item as completed.")
+    todo_name = input("Enter the todo name to mark as completed: ")
+    todo_lists = []
+    with open(file_name, "r") as f:
+        reader = csv.reader(f)
+        for row in reader: 
+            if (todo_name != row[0]): 
+                todo_lists.append(row)
+            else: 
+                todo_lists.append([row[0], "True"])
+    with open(file_name, "w") as f: 
+        writer = csv.writer(f)
+        writer.writerows(todo_lists)
 
 def view_todo(file_name):
     print("View TODO.")
